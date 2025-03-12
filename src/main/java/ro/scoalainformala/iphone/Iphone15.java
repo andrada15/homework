aa922384864c2dc99ba2db6fc684aaa03d91dab7
unknown <andrada.puisor@gmail.com>
2025-03-12 19:41:23 +0200

    Removed files

--- src/main/java/ro/scoalainformala/iphone/Iphone15.java
+++
@@ -1,9 +0,0 @@
-package ro.scoalainformala.iphone;
-
-import ro.scoalainformala.Phone;
-
-public class Iphone15 extends Phone {
-    public Iphone15(int batteryLife, String color, String material, String IMEI) {
-        super(batteryLife, color, material, IMEI);
-    }
-}
